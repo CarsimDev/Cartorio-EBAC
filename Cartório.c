ef8c0fbfd216a7d51701beea77dbb0ef2e6ddfc2
CarsimDev <143037328+CarsimDev@users.noreply.github.com>
2023-08-23 18:25:26 -0300

    Alteração para saída do usuário
    Foi incluído ao sistema a opção de sair do programa.

--- Cartório.c
+++ Cartório.c
@@ -130,7 +130,8 @@
   	    printf("Escolha a opção desejada do menu.\n\n");
  	    printf("\t1 - Registrar nomes\n");
  	    printf("\t2 - Consultar nomes\n");
- 	    printf("\t3 - Deletar nomes\n\n"); 
+ 	    printf("\t3 - Deletar nomes\n"); 
+ 	    printf("\t4 - Fechar programa\n");
 	    printf("Opção:"); //Fim do menu
  	
  	    scanf("%d",&opcao); //Armazenando info. do usuário
@@ -149,6 +150,11 @@
 	        
 	        case 3:
 	        deletar();
+	        break;
+	        
+	        case 4:
+	        printf("Obrigado por utilizar nosso programa!\n");
+	        return 0;
 	        break;
 	        
 	        default:
